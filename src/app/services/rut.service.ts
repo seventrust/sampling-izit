18206b60206e01acdd0aa1813904b9b49af19214
Yerffrey <yromero@izit.cl>
2020-09-25 15:07:40 -0300

    Actualizacion: Correciones menores

--- src/app/services/rut.service.ts
+++ src/app/services/rut.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { validate, clean, format } from 'rut.js';
 
 interface ErrorValidate {
@@ -11,23 +11,20 @@ interface ErrorValidate {
 export class RutService {
 	constructor() {}
 
-	public rutValidate(control: FormControl): ErrorValidate {
+	public rutValido(control: FormControl): ErrorValidate {
 		if (!control.value) {
 			return null;
 		}
 
 		let cleanValue: string = clean(control.value);
-		console.log(cleanValue);
 
 		let esValido: boolean = validate(cleanValue);
 
-		console.log(`Es un rut valido? -> ${esValido}`);
 		if (esValido) {
 			let formateado: string = format(cleanValue);
-
 			control.setValue(formateado);
 			return {
-				correcto: true,
+				rutValido: true,
 			};
 		} else {
 			return null;
